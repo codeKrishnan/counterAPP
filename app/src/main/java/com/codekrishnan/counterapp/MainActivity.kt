9d0f557934ed176bbada093af8f91a50f0cd675b
Gopi <sgopikrishnan1@gmail.com>
2022-02-11 13:49:13 +0530

    counterAPP - Initial commit

--- app/src/main/java/com/codekrishnan/counterapp/MainActivity.kt
+++ app/src/main/java/com/codekrishnan/counterapp/MainActivity.kt
@@ -2,11 +2,33 @@ package com.codekrishnan.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codekrishnan.counterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private var count: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding) {
+            buttonIncrement.setOnClickListener {
+                count++
+                updateCount()
+            }
+            buttonDecrement.setOnClickListener {
+                if (count > 0) {
+                    count--
+                    updateCount()
+                }
+            }
+        }
+    }
+
+    private fun updateCount() {
+        binding.textviewCount.text = count.toString()
     }
 }
